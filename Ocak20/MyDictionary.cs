36f35127f1f5f420273fa30594213234ef465deb
Saide Işılay Baykal <saide.baykal@istanbul.edu.tr>
2021-02-17 05:11:06 +0300

    Dict kavramı

---
+++ Ocak20/MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ocak20
+{
+    class MyDictionary<TKey,TValue> // where T : class
+    {
+        TKey[] keys;
+        TValue[] values;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempArrKey = keys;
+            keys = new TKey[keys.Length + 1];
+            TValue[] tempArrValue = values;
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempArrKey.Length; i++)
+            {
+                keys[i] = tempArrKey[i];
+                values[i] = tempArrValue[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+
+        public void List()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + " : " + values[i]);
+            }
+        }
+    }
+}
---
+++ Ocak20/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ocak20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------SÖZLÜK KAVRAMI---------\n");
+            MyDictionary<string, string> sozlükTDK = new MyDictionary<string, string>();
+            sozlükTDK.Add("sevmek", "Sevgi ve bağlılık duymak");
+            sozlükTDK.Add("hissetmek", "Fiziksel bir uyarıyı duymak");
+            sozlükTDK.Add("söz etmek", "bir şey üzerine konuşmak");
+            Console.WriteLine("-------{0} fiil LİSTESİ----------",sozlükTDK.Count);
+            sozlükTDK.List();
+
+            
+            sozlükTDK.Add("ilgi", "İki şey arasında bulunan herhangi bir bağlılık, ilişki, alaka, taalluk, aidiyet");
+            sozlükTDK.Add("yergi", "Bir kimseyi, bir toplumu, bir düşünceyi, bir nesneyi, bir göreneği yermek için yazılmış yazı veya söylenmiş söz, hicviye, hiciv, satir");
+            sozlükTDK.Add("sevgi", "İnsanı bir şeye veya bir kimseye karşı yakın ilgi ve bağlılık göstermeye yönelten duygu");
+            Console.WriteLine("--------{0} SON LİSTE---------", sozlükTDK.Count);
+            sozlükTDK.List();
+        }
+    }
+}
